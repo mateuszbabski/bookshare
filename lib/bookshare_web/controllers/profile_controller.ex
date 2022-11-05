291c476bb03ebbb0e36a4a77ade26fe51ed97b50
Mateusz <babskimateusz@gmail.com>
2022-11-05 13:22:38 +0100

    create profile controller created

--- lib/bookshare_web/controllers/profile_controller.ex
+++ lib/bookshare_web/controllers/profile_controller.ex
@@ -6,38 +6,46 @@ defmodule BookshareWeb.ProfileController do
 
   action_fallback BookshareWeb.FallbackController
 
-  def index(conn, _params) do
-    profiles = Accounts.list_profiles()
-    render(conn, "index.json", profiles: profiles)
-  end
+  # def index(conn, _params) do
+  #   profiles = Accounts.list_profiles()
+  #   render(conn, "index.json", profiles: profiles)
+  # end
 
-  def create(conn, %{"profile" => profile_params}) do
-    with {:ok, %Profile{} = profile} <- Accounts.create_profile(profile_params) do
+  def create(conn, profile_params) do
+    user = conn.assigns.current_user
+
+    if Accounts.check_if_user_has_profile(user.id) do
       conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.profile_path(conn, :show, profile))
-      |> render("show.json", profile: profile)
-    end
+      |> put_status(:forbidden)
+      |> json(%{message: "You are allowed to have only one profile"})
+    else
+      with {:ok, %Profile{} = profile} <- Accounts.create_profile(user, profile_params) do
+        conn
+        |> put_status(:created)
+        |> json(%{username: profile.username, message: "Profile created"})
+      end
   end
+  # def create(conn, profile_params) do
+  #   user = conn.assigns.current_user
 
-  def show(conn, %{"user_id" => user_id}) do
-    profile = Accounts.get_profile!(user_id)
-    render(conn, "show.json", profile: profile)
-  end
+  #   with {:ok, %Profile{} = profile} <- Accounts.create_profile(user, profile_params) do
+  #     conn
+  #     |> put_status(:created)
+  #     |> json(%{username: profile.username, message: "Profile created"})
+  #   end
 
-  def update(conn, %{"id" => id, "profile" => profile_params}) do
-    profile = Accounts.get_profile!(id)
+  # def show(conn, %{"user_id" => user_id}) do
+  #   profile = Accounts.get_profile!(user_id)
+  #   render(conn, "show.json", profile: profile)
+  # end
 
-    with {:ok, %Profile{} = profile} <- Accounts.update_profile(profile, profile_params) do
-      render(conn, "show.json", profile: profile)
-    end
-  end
+  # def update(conn, %{"id" => id, "profile" => profile_params}) do
+  #   profile = Accounts.get_profile!(id)
 
-  def delete(conn, %{"id" => id}) do
-    profile = Accounts.get_profile!(id)
+  #   with {:ok, %Profile{} = profile} <- Accounts.update_profile(profile, profile_params) do
+  #     render(conn, "show.json", profile: profile)
+  #   end
+  # end
 
-    with {:ok, %Profile{}} <- Accounts.delete_profile(profile) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+end
 end
